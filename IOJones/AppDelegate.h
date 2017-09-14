cf8950730cf97a39eee90ae590a7b9e4fc9f6688
Jief <github-prf@jf-luce.name>
2017-09-14 09:20:12 +0200

    Commit source from  https://git.code.sf.net/p/iojones/code iojones-code as they are the 14th of Sep 2017

---
+++ IOJones/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  IOJones
+//
+//  Created by PHPdev32 on 3/13/13.
+//  Licensed under GPLv3, full text at http://www.gnu.org/licenses/gpl-3.0.txt
+//
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@end
+
+@interface ArrayTransformer : NSValueTransformer
+
+@end
+
+@interface ShouldEnable : NSValueTransformer
+
+@end
